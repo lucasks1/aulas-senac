9d776afd4ed4eaaf16c4533995e109221d4c240f
Lucas <116185440+lucasks1@users.noreply.github.com>
2023-03-07 22:30:45 -0300

    Prova js
    senac

---
+++ Aula JavaScript/ProvaJS/prov16.js
@@ -0,0 +1,27 @@
+maça = 1,30
+maça2 = 1
+ 
+compra = parseFloat(prompt("Quantas maças voce vai levar"))
+
+if (compra>12) {
+    soma = compra*maça2 
+    console.log("valor a pagar",soma)
+
+}else{
+    soma2 = compra*maça 
+    console.log("valor a pagar",soma2)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
