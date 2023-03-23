c6807e910597696d3eb13ade29475b00c57ae2e9
Lucas <balahallslemos@gmail.com>
2023-03-23 09:03:39 -0300

    senac
    consertado algumas coisas

--- HTMLL5 e CSS3/CSS E HTML E JS/exercícios js e visuG/aula2/data.js
+++
@@ -1,6 +0,0 @@
-testadata = new Date()
-mes = testadata.getMonth()+1
-ano = testadata.getFullYear()
-console.log(testadata)
-console.log("mes: "+ mes)
-console.log("ano: "+ ano)                                                                                                                     
--- HTMLL5 e CSS3/CSS E HTML E JS/exercícios js e visuG/aula2/imput.js
+++
@@ -1,5 +0,0 @@
-n1= parseFloat(prompt("Digite o primeiro número"))
-n2 = parseInt(prompt("digite o segundo número"))
-total = parseFloat (n1+n2)
-console.log("Resultado: " + total)
-
