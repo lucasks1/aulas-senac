20a9eaa062c3e7e12b455ef8e51f6b27a0c79d20
Lucas Lemos <balahallslemos@gmail.com>
2023-07-17 09:35:07 -0300

    calculadora

---
+++ ScriptsWEb/Atividades (CORREÇÃO)/carro.js
@@ -0,0 +1,20 @@
+var Carro = /** @class */ (function () {
+    function Carro() {
+        this.nome = "Fusca";
+        this.marca = "vw";
+        this.ano = 1870;
+    }
+    return Carro;
+}());
+var car;
+car = new Carro();
+car.nome = "Fusca Azul";
+car.marca = "Volkswagen";
+car.ano = 1986;
+console.log("Carro: ", car);
+var carrao;
+carrao = new Carro;
+//carrao.nome = "Alvio Custuzio";
+//carrao.marca = "Ferrari";
+//carrao.ano = 1810;
+console.log("Carro: ", carrao);
