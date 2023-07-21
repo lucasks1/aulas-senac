58c6ce8e14b61edb59358d3ebaf37a8b520f777d
Lucas Lemos <balahallslemos@gmail.com>
2023-07-21 11:35:20 -0300

    arrumado aula 04

--- ScriptsWEb/Aula04 Angular/primeiroProjeto.2/src/app/app.module.ts
+++ ScriptsWEb/Aula04 Angular/primeiroProjeto.2/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
 import { InterpolacaoComponent } from './page/interpolacao/interpolacao.component';
 import { CondicionalComponent } from './page/condicional/condicional.component';
 import { NavbarComponent } from './charepage/navbar/navbar.component';
+import { PipeComponent } from './page/pipe/pipe.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +22,14 @@ import { NavbarComponent } from './charepage/navbar/navbar.component';
     HomeComponent,
     InterpolacaoComponent,
     CondicionalComponent,
-    NavbarComponent
+    NavbarComponent,
+    PipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
